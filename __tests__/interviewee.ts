73185878fe6d8b01e269a5643e6b44aeceb0430d
Mateus Brum <mateushenriquebrum@gmail.com>
2021-03-01 12:24:12 +0000

    Interviwee fetch calendar by token

--- __tests__/interviewee.ts
+++ __tests__/interviewee.ts
@@ -1,22 +1,32 @@
-import { FetchInterviewerCalendarByToken } from '../src/interviewee';
+import { FetchIntervieweeCalendarByToken, IntervieweeCalenderRepository } from '../src/interviewee';
 import { instance, anything, mock, when } from 'ts-mockito';
+import { Free } from '../src/slot';
 
+let mockRep = mock<IntervieweeCalenderRepository>()
 
 beforeEach(() => {
-
+    let mockRep = mock<IntervieweeCalenderRepository>()
 });
 
-describe("Interviewer offer a token to fetch its calendar", () => {
+describe("Interviewee fetch a calendar by token", () => {
    
-    it("Then interviewee fetch it all", async () => {
-        const fetch = new FetchInterviewerCalendarByToken()
+    it("Then interviewee fetch it all", async () => {        
+        when(mockRep.slotsByToken("token")).thenResolve([new Free(new Date(0), new Date(1))])
+        const fetch = new FetchIntervieweeCalendarByToken(instance(mockRep))
+        const slots = (await fetch.execute("token")).ok
+        expect(slots).not.toBeNull()
+        expect(slots.length).toBe(1);
     });
 
-    it("Then interviewee pick one free slot", async () => {
+    it("Then interviewee pick a free slot", async () => {
 
     });
 
-    it("Then interviewee pick one taken slot", async () => {
+    it("Then interviewee pick a taken slot", async () => {
+
+    });
+
+    it("Then interviewee try to pick another slot with the same token", async () => {
 
     });
 });
--- src/interviewee.ts
+++ src/interviewee.ts
@@ -1,6 +1,19 @@
-export class FetchInterviewerCalendarByToken {
-    constructor() {
+import { Free, Slot } from "./slot";
+import {Result, Ok, Error} from './shared'
 
-    }
-    
+export type Token = String
+
+export interface IntervieweeCalenderRepository {
+    slotsByToken(token: Token): Promise<Array<Free>>
+}
+
+export class FetchIntervieweeCalendarByToken {
+    constructor(private rep: IntervieweeCalenderRepository) {}
+
+    async execute(token: Token) : Promise<Result<Array<Slot>>> {
+        //should verify token and ensure not be used again, something like:
+        //if(token.isWellFormat && rep.slotsByToken(token).length == 1)
+        const slots = (await this.rep.slotsByToken(token))
+        return new Ok(slots);
+    }    
 }
