50b3df16242db57e1188bdac03c4ec9b3884e5e8
Mateus Brum <mateushenriquebrum@gmail.com>
2021-03-01 12:27:57 +0000

    improve test

--- __tests__/interviewee.ts
+++ __tests__/interviewee.ts
@@ -1,5 +1,5 @@
 import { FetchIntervieweeCalendarByToken, IntervieweeCalenderRepository } from '../src/interviewee';
-import { instance, anything, mock, when } from 'ts-mockito';
+import { instance, anything, mock, when, verify } from 'ts-mockito';
 import { Free } from '../src/slot';
 
 let mockRep = mock<IntervieweeCalenderRepository>()
@@ -9,17 +9,22 @@ beforeEach(() => {
 });
 
 describe("Interviewee fetch a calendar by token", () => {
+
+    const onlyFreeSlot = new Free(new Date(0), new Date(1))
+    const token = "any_token";
    
     it("Then interviewee fetch it all", async () => {        
-        when(mockRep.slotsByToken("token")).thenResolve([new Free(new Date(0), new Date(1))])
+        when(mockRep.slotsByToken(token)).thenResolve([onlyFreeSlot])
         const fetch = new FetchIntervieweeCalendarByToken(instance(mockRep))
-        const slots = (await fetch.execute("token")).ok
+        const slots = (await fetch.execute(token)).ok
         expect(slots).not.toBeNull()
         expect(slots.length).toBe(1);
+        verify(mockRep.slotsByToken(token)).once()
     });
 
     it("Then interviewee pick a free slot", async () => {
-
+        //const pick = new PickFreeSlotByToken(instance(mockRep))
+        //const confirm = (await pick.execute(token, onlyFreeSlot))
     });
 
     it("Then interviewee pick a taken slot", async () => {
