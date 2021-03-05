73425a22417d2854b20d03d66ec096dd92affb9a
Mateus Brum <mateushenriquebrum@gmail.com>
2021-03-05 20:38:50 +0000

    basic auth endpoint

--- src/domain/authentication.ts
+++ src/domain/authentication.ts
@@ -1,8 +1,8 @@
+import { Email, Ok, Result, Error, Token } from "./shared";
+
 var jwt = require('jsonwebtoken');
 type JWT = string; // should be nice to be able to define constraints here like .*{12}.*{12}.*{12}, because the type here is not only a string but a well know formated string.
-export class Authentication {
-    // Some repository where you can find the user by user and pass
-    // That is trivial, not implemented here by the sake of time
+class Tokens {
     private secret;
     constructor() {
         this.secret = "some_private_key";
@@ -19,6 +19,32 @@ export class Authentication {
         } catch (e) {
             console.log("verify", e)
             return false
+        }
+    }
+}
+
+export interface UserRepository {
+    findUserByEmailAndSecret(user: User): User
+}
+
+export class MockUserRepository implements UserRepository {
+    findUserByEmailAndSecret(user: User): User {
+        return user;
+    }
+}
+type User = {email: Email, secret: string};
+export class InterviewerLogin {
+    // Some repository where you can find the user by user and pass
+    // That is trivial, not implemented here by the sake of time    
+    constructor(private rep: UserRepository) {}
+
+    async execute(user: User): Promise<Result<Token>> {
+        const found = this.rep.findUserByEmailAndSecret(user);
+        if(found) {
+            const token = await new Tokens().tokenFor(user.email)
+            return new Ok(token);
+        }else{
+            return new Error(["User not found"]);
         }
     }
 }
--- src/infrastructure/app.ts
+++ src/infrastructure/app.ts
@@ -1,7 +1,24 @@
 import express from 'express';
+const bodyParser = require('body-parser')
+
+import { InterviewerLogin, MockUserRepository } from "../domain/authentication"
 const app = express();
+app.use(bodyParser.json())
 const PORT = 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
+app.post('/auth', async (req, res) => {
+  console.log(req)
+  const { email, secret } = req.body
+  //should have some factory by env (dev, test, prod)
+  const usecase = new InterviewerLogin(new MockUserRepository())
+  const result = await usecase.execute({email, secret});
+  if (result.error.length) {
+    res.status(404).send(result.error[0].toString());
+  } else {
+    res.json({ token: result.ok });
+  }
+
+});
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at https://localhost:${PORT}`);
 });
